185b62ccce2686947b071d724159eff4b4e7acba
Marco Lo Bello <xSalamander@outlook.it>
2023-02-21 17:15:21 +0100

    Parametrized and adapted for 10'' tablet

--- app/src/main/java/com/project1/cookbook/Dishes.kt
+++ app/src/main/java/com/project1/cookbook/Dishes.kt
@@ -1,4 +1,3 @@
 package com.project1.cookbook
 
-data class Dishes (val image: Int, val title: String, val background: Int
-    )
+data class Dishes (val image: Int, val title: String, val background: Int)
--- app/src/main/java/com/project1/cookbook/ReadRecipesFragment.kt
+++ app/src/main/java/com/project1/cookbook/ReadRecipesFragment.kt
@@ -21,8 +21,6 @@ class ReadRecipesFragment : Fragment() {
 
     private var _binding: FragmentReadRecipesBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
