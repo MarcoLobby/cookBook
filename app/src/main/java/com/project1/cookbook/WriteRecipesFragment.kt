6307a147e8219366b896856ff5e7e8c207acaf25
Marco Lo Bello <xSalamander@outlook.it>
2023-02-22 14:20:30 +0100

    Created Basic Insert text in write recipes fragment

--- app/src/main/java/com/project1/cookbook/WriteRecipesFragment.kt
+++ app/src/main/java/com/project1/cookbook/WriteRecipesFragment.kt
@@ -1,40 +1,45 @@
 package com.project1.cookbook
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import com.project1.cookbook.databinding.FragmentWriteRecipesBinding
 
 
 class WriteRecipesFragment : Fragment() {
 
     private var _binding: FragmentWriteRecipesBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         _binding = FragmentWriteRecipesBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_writeRecipesFragment_to_HomeFragment)
+        binding.actionButton.setOnClickListener {
+            binding.tvTitleShow.text = binding.tvInsertTitle.text
+            Snackbar.make(binding.actionButton, binding.tvInsertTitle.text, Snackbar.LENGTH_INDEFINITE).show()
+        }
+
+        //MIN CHAR LENGTH TO LET CONFIRM BUTTON ENABLE
+        binding.tvInsertTitle.doOnTextChanged { text, start, before, count ->
+            Log.v("WriteRecipes","insert title: text = ${text}, start = ${start}, before= ${before}, count= $count")
+            text?.let {
+                binding.actionButton.isEnabled = it.length >=3
+            }
         }
     }
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
