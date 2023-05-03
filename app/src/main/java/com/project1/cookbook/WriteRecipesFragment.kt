33e96f0ac20d02f0839b248a11dbcadf14557c0f
Marco Lo Bello <xSalamander@outlook.it>
2023-05-03 17:07:03 +0200

    Added Shared Preferences in the Write recipe fragment

--- app/src/main/java/com/project1/cookbook/WriteRecipesFragment.kt
+++ app/src/main/java/com/project1/cookbook/WriteRecipesFragment.kt
@@ -1,10 +1,13 @@
 package com.project1.cookbook
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import com.project1.cookbook.databinding.FragmentWriteRecipesBinding
@@ -14,6 +17,8 @@ class WriteRecipesFragment : Fragment() {
 
     private var _binding: FragmentWriteRecipesBinding? = null
     private val binding get() = _binding!!
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
@@ -29,6 +34,13 @@ class WriteRecipesFragment : Fragment() {
 
         }
 
+        loadData()
+
+        binding.actionButton.setOnClickListener {
+            saveData()
+        }
+
+
         //MIN CHAR LENGTH TO LET CONFIRM BUTTON ENABLE
         binding.tvInsertTitle.doOnTextChanged { text, start, before, count ->
             Log.v(
@@ -39,6 +51,27 @@ class WriteRecipesFragment : Fragment() {
                 binding.actionButton.isEnabled = it.length >= 3
             }
         }
+    }
+
+    private fun saveData() {
+        val inputCamp = binding.tvInsertTitle.text.toString()
+        binding.tvTitleShow.text = inputCamp
+
+        val sharedPreferences : SharedPreferences = requireContext().getSharedPreferences("sharedPref", Context.MODE_PRIVATE)
+        val editor : SharedPreferences.Editor = sharedPreferences.edit()
+        editor.apply {
+            putString("KEY_STRING", inputCamp)
+        }.apply()
+
+    }
+
+    private fun loadData(){
+        val sharedPreferences : SharedPreferences = requireContext().getSharedPreferences("sharedPref", Context.MODE_PRIVATE)
+        val savedString : String? = sharedPreferences.getString("KEY_STRING", null)
+
+
+        binding.tvTitleShow.text = savedString
+
     }
 
     override fun onDestroyView() {
